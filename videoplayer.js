5b54f1f9349cc71f84f71f6d1b78f79c0dda829b
+ <+>
2024-08-22 23:19:54 +0630

    colors-changed

--- videoplayer.js
+++ videoplayer.js
@@ -1,10 +1,10 @@
-window.onload = function() {
+window.onload = function () {
     const video = document.getElementById('myVideo');
     video.play();
 };
 
 // Optional: Handle full-screen behavior on user interaction
-document.getElementById('myVideo').addEventListener('click', function() {
+document.getElementById('myVideo').addEventListener('click', function () {
     if (this.requestFullscreen) {
         this.requestFullscreen();
     } else if (this.webkitRequestFullscreen) { // Safari support
